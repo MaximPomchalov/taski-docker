b57198c66f14854a0cd5c3f057c740eb81a41400
Maxim Pomchalov <xipo23@yandex.ru>
2025-07-18 20:34:50 +0300

    add domen

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['51.250.109.196', '123.123.123.123', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['51.250.109.196', '123.123.123.123', '127.0.0.1', 'localhost', 'taskiprod.ddns.net']
 
 # Application definition
 
